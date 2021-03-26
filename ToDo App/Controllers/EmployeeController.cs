05b7d5ec293fb317b64a72a26f82fca0ba70b7c4
Priyanshu <priyanshu.rajrahul.mec17@iitbhu.ac.in>
2021-03-26 23:00:41 +0530

    Change

---
+++ ToDo App/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo_App.Models;
+
+namespace ToDo_App.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DapperORM.ReturnList<EmployeeModel>("EmployeeViewAll"));
+        }
+        //  ../Employee/Edit - insert
+        //  ../Employee/AddorEdit/id
+        [HttpGet]
+        public ActionResult AddorEdit(int id=0)
+        {
+            if (id == 0)
+                return View();
+            else
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@EmployeeID", id);
+                return View(DapperORM.ReturnList<EmployeeModel>("EmployeeViewByID",param).FirstOrDefault<EmployeeModel>());
+            }
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(EmployeeModel emp)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", emp.EmployeeID);
+            param.Add("@Name", emp.Name);
+            param.Add("@Position", emp.Position);
+            param.Add("@Age", emp.Age);
+            param.Add("@Salary", emp.Salary);
+
+            DapperORM.ExecuteWithoutReturn("EmployeeAddorEdit", param);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("EmployeeID", id);
+            DapperORM.ExecuteWithoutReturn("EmployeeDeletebyID", param);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDo App/Models/DapperORM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+
+namespace ToDo_App.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionString = @"Data Source=DESKTOP-NSERG95;Initial Catalog=DapperDB;Integrated Security=True;";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType:
+                    CommandType.StoredProcedure),typeof(T));
+            }
+        }
+        //DapperORM.ReturnList<EmployeeModel> <= IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
